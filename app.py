8cc6776f8a9520cfe6dfd7e68b445cbd8105be9b
Maha-I-Hendawy <maha.i.hendawy@gmail.com>
2024-05-10 21:18:16 +0200

    added some stuff

--- app.py
+++ app.py
@@ -1,10 +1,45 @@
-from flask import Flask, render_template, request, redirect, url_for, session 
+from flask import Flask, render_template, request, redirect, url_for, flash, session 
+from flask_sqlalchemy import SQLAlchemy 
+from werkzeug.security import generate_password_hash, check_password_hash
 import nltk
 
 
 app = Flask(__name__, instance_relative_config=True)
 app.config.from_object('config')
 app.config.from_pyfile('config.py')
+
+db = SQLAlchemy(app)
+
+
+app.app_context().push()
+
+
+class User(db.Model):
+	user_id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(100), nullable=False, unique=True)
+	email = db.Column(db.String(100), nullable=False, unique=True)
+	password = db.Column(db.String(100), nullable=False)
+	text = db.relationship('Text', backref='user', lazy=True)
+
+
+
+	def __str__(self):
+		return f"{self.username}, {self.email}"
+
+
+
+class Text(db.Model):
+	text_id = db.Column(db.Integer, primary_key=True)
+	text = db.Column(db.Text())
+	user_id = db.Column(db.Integer, db.ForeignKey('user.user_id'), nullable=False)
+
+
+	def __str__(self):
+		return f"{self.text}"
+
+
+
+
 
 
 @app.route('/')
@@ -15,14 +50,25 @@ def home():
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+	users = User.query.all()
 	if request.method == 'POST':
 		username = request.form.get("username")
 		email = request.form.get("email")
 		password = request.form.get("password")
 		confirm_password = request.form.get("confirm_password")
+		for user in users:
+			if user.username == username:
+				flash("Username already exists")
+			if user.email == email:
+				flash("Email already exists")
 
 		if password == confirm_password:
-			pass
+			hashed_password = generate_password_hash(password)
+			user = User(username=username, email=email, password=hashed_password)
+			db.session.add(user)
+			db.session.commit()
+			return redirect(url_for('login'))
+			
 	return render_template("register.html", title="Registration Page")
 
 
@@ -30,14 +76,38 @@ def register():
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+	if request.method == 'POST':
+		username = request.form.get("username")
+		password = request.form.get("password")
+
+		user = User.query.filter_by(username=username).first()
+
+		if user and check_password_hash(user.password, password):
+			session["user"] = user.username
+			flash(f"Welcome {user.username}")
+			return redirect(url_for('profile'))
+
 	return render_template("login.html", title="Login Page")
 
 
 
 @app.route('/logout')
 def logout():
-	pass
+	if 'user' in session:
+		session.pop('user', None)
+		return redirect(url_for('login'))
+	else:
+		return redirect(url_for('login'))
 
+
+
+
+@app.route('/profile')
+def profile():
+	if 'user' in session:
+		return render_template("profile.html")
+	else:
+		return redirect(url_for('login'))
 
 
 words = []
